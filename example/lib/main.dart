1f50ee31d5b8584fe990d853373c697bfea331fd
Kathirvel Chandrasekaran <kathirvel98@outlook.com>
2023-01-16 13:07:08 +0530

    feat: added util function to generate words

---
+++ example/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:word_generator/word_generator.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  helloFunction() {
+    var data = WordGenerator().countSyllables('kathirvel');
+    print(data);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: Center(
+          child: GestureDetector(
+            onTap: () => helloFunction(),
+            child: const Text('Hello World'),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/word_generator.dart
@@ -0,0 +1,68 @@
+library word_generator;
+
+import 'dart:math';
+
+import 'package:word_generator/nouns.dart';
+
+class WordGenerator {
+  /// Returns a list of random words.
+  /// [count] is the number of words to return.
+  randomNouns([int count = 1]) {
+    final random = Random();
+    final nouns = <String>[];
+    for (var i = 0; i < count; i++) {
+      var noun = '';
+      for (var j = 0; j < count; j++) {
+        noun += nouns[random.nextInt(nouns.length)];
+      }
+      nouns.add(noun);
+    }
+    return nouns;
+  }
+
+  /// Returns a random noun.
+  randomNoun() {
+    final random = Random();
+    return nouns[random.nextInt(nouns.length)];
+  }
+
+  /// Returns a random sentence.
+  /// [count] is the number of nouns to return.
+  randomSentence([int count = 2]) {
+    final random = Random();
+    var sentence = '';
+    for (var i = 0; i < count; i++) {
+      sentence += '${nouns[random.nextInt(nouns.length)]} ';
+    }
+    return sentence;
+  }
+
+  /// count the syllables in a noun
+  /// [noun] is the noun to count the syllables in.
+  countSyllables(String noun) {
+    var count = 0;
+    var vowels = 'aeiouy';
+    noun = noun.toLowerCase().trim();
+    if (noun.isEmpty) {
+      return count;
+    }
+    if (noun.length == 1) {
+      return count;
+    }
+    if (vowels.contains(noun[0])) {
+      count++;
+    }
+    for (var i = 1; i < noun.length; i++) {
+      if (vowels.contains(noun[i]) && !vowels.contains(noun[i - 1])) {
+        count++;
+      }
+    }
+    if (noun.endsWith('e')) {
+      count--;
+    }
+    if (count == 0) {
+      count++;
+    }
+    return count;
+  }
+}
