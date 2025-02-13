5771582fa576c62dae779b15b8d0bdde036ade49
diederik <diederikjhattingh@gmail.com>
2025-02-13 20:45:24 +0200

    Fix verbs and names appending incorrectly in double loop.
    Added tests.
    
    Fixes KathirvelChandrasekaran/word_generator#9

--- test/word_generator_test.dart
+++ test/word_generator_test.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:word_generator/word_generator.dart';
+import 'package:word_generator/data/verbs.dart' as verbs_data;
+import 'package:word_generator/data/names.dart' as names_data;
 
 void main() {
   test('randomNoun', () {
@@ -20,5 +22,29 @@ void main() {
   test('randomNouns', () {
     final wordGenerator = WordGenerator();
     expect(wordGenerator.randomNouns(5).length, 5);
+  });
+
+  test('randomNames', () {
+    final wordGenerator = WordGenerator();
+    expect(wordGenerator.randomNames(5).length, 5);
+  });
+
+  test('randomNamesNotAppended', () {
+    final wordGenerator = WordGenerator();
+    final names = wordGenerator.randomNames(5);
+
+    expect(names.every((name) => names_data.names.contains(name)), isTrue);
+  });
+
+   test('randomVerbs', () {
+    final wordGenerator = WordGenerator();
+    expect(wordGenerator.randomVerbs(5).length, 5);
+  });
+
+  test('randomVerbsNotAppended', () {
+    final wordGenerator = WordGenerator();
+    final verbs = wordGenerator.randomVerbs(5);
+
+    expect(verbs.every((verb) => verbs_data.verbs.contains(verb)), isTrue);
   });
 }
